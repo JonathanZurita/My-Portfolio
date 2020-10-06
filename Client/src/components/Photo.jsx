fd37705de289b4842228f61da391ebfdee4205d6
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-10-05 22:01:13 -0500

    modal styling

--- Client/src/components/Photo.jsx
+++ Client/src/components/Photo.jsx
@@ -14,8 +14,8 @@ const Photo = ({ handleToggle, bool, photoID}) => {
               <img className="imageOnModal" src={photoID.url} />
             </div>
             <p className="photoDeats" id="picDesc">{photoID.description}</p>
-            <p className="photoDeats" id="picDate">{photoID.date}</p>
-            <p className="photoDeats" id="picLoc">{photoID.city} {photoID.state}</p>
+            <p className="photoDeats" id="picDate"><span><i class="fas fa-calendar-day"></i></span> <span>{photoID.date}</span></p>
+            <p className="photoDeats" id="picLoc"> <span><i class="fas fa-map-marker-alt"></i></span> <span>{photoID.city} {photoID.state}</span></p>
           
           </div>
         </div>
