fa771176274b33b49a69c4794f8a27d34ccc202d
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-10-06 00:25:14 -0500

    updated icons on image modal

--- Client/src/components/Photo.jsx
+++ Client/src/components/Photo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Photo = ({ handleToggle, bool, photoID}) => {
-
+  var icons = [];
+  
   if(bool) {
+    if(photoID.city.length > 0 && photoID.state.length > 0) {
+      icons.push("fas fa-calendar-day", "fas fa-map-marker-alt");
+      
+    }
     return(
       <div 
           className="modalWrap" 
@@ -14,8 +19,8 @@ const Photo = ({ handleToggle, bool, photoID}) => {
               <img className="imageOnModal" src={photoID.url} />
             </div>
             <p className="photoDeats" id="picDesc">{photoID.description}</p>
-            <p className="photoDeats" id="picDate"><span><i class="fas fa-calendar-day"></i></span> <span>{photoID.date}</span></p>
-            <p className="photoDeats" id="picLoc"> <span><i class="fas fa-map-marker-alt"></i></span> <span>{photoID.city} {photoID.state}</span></p>
+            <p className="photoDeats" id="picDate"><span><i class={icons[0]}></i></span> <span>{photoID.date}</span></p>
+            <p className="photoDeats" id="picLoc"> <span><i class={icons[1]}></i></span> <span>{photoID.city} {photoID.state}</span></p>
           
           </div>
         </div>
